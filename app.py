a274ef8882391f9b3b6482fd1213f21d1d4ccd32
Abdelouafi09 <97417156+Abdelouafi09@users.noreply.github.com>
2023-07-11 14:16:46 +0000

    added logo to nav and login form

--- app.py
+++ app.py
@@ -36,5 +36,10 @@ def st_home():
   return render_template('st_home.html')
 
 
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
